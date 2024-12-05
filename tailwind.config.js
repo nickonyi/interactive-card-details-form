d00b6e14063b3a02206dbdd7bbdfbada2193a7ad
phatommobb <nickoluoch1130@gmail.com>
2024-12-05 13:35:08 +0300

    HTML and CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./dist/assets/images/bg-main-desktop.png')",
+      },
+    },
   },
-  plugins: { tailwindcss: {}, autoprefixer: {} },
+  plugins: [],
 };
