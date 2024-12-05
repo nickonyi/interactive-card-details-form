257669a6ec0dbd1c65e29c53d026be65ccf0c5eb
phatommobb <nickoluoch1130@gmail.com>
2024-12-05 13:05:35 +0300

    HTML and CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,css}'],
-  // ...
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: { tailwindcss: {}, autoprefixer: {} },
 };
