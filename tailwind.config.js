f40ced7207990f35adac4a1acdfbf60dfeb8d280
phatommobb <nickoluoch1130@gmail.com>
2024-12-07 17:10:29 +0300

    HTML and CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         'hero-card-front': "url('assets/images/bg-card-front.png')",
         'hero-card-back': "url('assets/images/bg-card-back.png')",
       },
+      border: {
+        '1p': '1px',
+      },
     },
   },
   plugins: [],
