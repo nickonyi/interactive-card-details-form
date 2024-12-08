1e074f5ac0428a285c577e7d61ffe8f9c4c3122f
phatommobb <nickoluoch1130@gmail.com>
2024-12-08 17:03:07 +0300

    HTML and CSS setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import { skaa } from './don';
 import './styles/style.css';
 
-const varo = 'holla';
+const form = document.querySelector('#form');
+const cardHolderName = document.querySelector('#cardholder-name');
+const cardNumber = document.querySelector('#card-number');
+const expMonth = document.querySelector('#expiry-month');
+const expYear = document.querySelector('#expiry-year');
+const cvcNo = document.querySelector('#cvc');
+const cardError = document.querySelector('#cardError');
 
-const dar = 'skaa';
+cardNumber.addEventListener('input', (e) => {
+  const input = e.target;
+  let inputValue = input.value.replace(/\s/g, '');
+  const formattedValue = inputValue.replace(/(.{4})/g, '$1 ');
+  input.value = formattedValue;
+});
 
-console.log(skaa);
+const validateForm = (e) => {
+  e.preventDefault();
+
+  console.log([cardHolderName, cardNumber, expMonth, expYear, cvcNo]);
+};
+
+form.addEventListener('submit', validateForm);
